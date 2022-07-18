05c03ec9fb47178bba9459037a8406d8c498eb89
adamascencio <ascencioadam@gmail.com>
2022-07-18 13:35:48 -0700

    added create functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,7 +2,8 @@ const Flight = require('../models/flight');
 
 module.exports = {
   index,
-  new: newFlight
+  new: newFlight, 
+  create,
 };
 
 function index(req, res) {
@@ -13,4 +14,13 @@ function index(req, res) {
 
 function newFlight(req, res) {
   res.render('flights/new');
+}
+
+function create(req, res) {
+  var flight = new Flight(req.body);
+  flight.save(function(err) {
+    if (err) return res.redirect('/flights/new');
+    console.log(flight);
+    res.redirect('/flights');
+  });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -6,5 +6,7 @@ const flightsCtrl = require('../controllers/flights');
 router.get('/', flightsCtrl.index);
 // GET /flights/new (new functionality - render form to add flights)
 router.get('/new', flightsCtrl.new);
+// POST /flights (create functionality - add new flight information to our model)
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
